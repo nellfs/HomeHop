1f293a568b80e645763a1be8d57bfd80cbd96e45
nellfs <pokycelit@gmail.com>
2022-01-22 13:23:20 -0300

    users

--- src/App.js
+++ src/App.js
@@ -21,14 +21,6 @@ const App = () => {
                   <FiSearch size={25} color={"#000"} />
                 </button>
               </div>
-
-              <main className="main">
-                <h3>ID: 000001</h3>
-                <span>Rua 38</span>
-                <span>Em frente ao mercado</span>
-                <span>Vila Nova</span>
-                <span>Cidade Grande - BR</span>
-              </main>
               <Users />
             </div>
           }
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>User Works!</h2>;
+  const USERS_DUMMY = [
+    {
+      id: "u1",
+      name: "Regina Figo",
+      image:
+        "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/microsoft/74/grinning-face_1f600.png",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS_DUMMY} />;
 };
 
 export default Users;
