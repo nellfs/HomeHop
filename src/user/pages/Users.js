49b69ee3d337c5a7a839914f8a82a74701cc68d6
nellfs <pokycelit@gmail.com>
2022-03-03 21:17:24 -0300

    http hook

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -3,55 +3,28 @@ import React, { useEffect, useState } from "react";
 import UsersList from "../components/UsersList";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
-
-// const USERS_DUMMY = [
-//   {
-//     id: "u1",
-//     name: "Alex Rocha",
-//     responsible: "Silvia Rocha",
-//     image: "https://thispersondoesnotexist.com/image",
-//   },
-//   {
-//     id: "u2",
-//     name: "Alex Rocha",
-//     responsible: "Silvia Rocha",
-//     image: "https://thispersondoesnotexist.com/image",
-//   },
-// ];
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
 const Users = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
   const [loadedUsers, setLoadedUsers] = useState();
 
   useEffect(() => {
-    const sendRequest = async () => {
-      setIsLoading(true);
+    const fetchUsers = async () => {
       try {
-        const response = await fetch("http://localhost:5000/api/users");
+        const responseData = await sendRequest(
+          "http://localhost:5000/api/users"
+        );
 
-        const respondeData = await response.json();
-
-        if (!response.ok) {
-          throw new Error(respondeData.message);
-        }
-
-        setLoadedUsers(respondeData.users);
-      } catch (err) {
-        setError(err.message);
-      }
-      setIsLoading(false);
+        setLoadedUsers(responseData.users);
+      } catch (err) {}
     };
-    sendRequest();
-  }, []);
-
-  const errorHandler = () => {
-    setError(null);
-  };
+    fetchUsers();
+  }, [sendRequest]);
 
   return (
     <>
-      <ErrorModal error={error} onClear={errorHandler} />
+      <ErrorModal error={error} onClear={clearError} />
       {isLoading && (
         <div className="center">
           <LoadingSpinner></LoadingSpinner>
